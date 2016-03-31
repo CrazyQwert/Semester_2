33b596b9e389025866a9fa7969c3936cd8f4f655
Arvid <a.buschsieweke@outlook.de>
2016-03-31 05:59:11 +0200

    Added code for exercise 2

---
+++ Semester_2/src/vorfuehrAufgabe1/Book.java
@@ -0,0 +1,120 @@
+package vorfuehrAufgabe1;
+
+public class Book {
+	
+	private String isbn;
+	private String title;
+	private String author;
+	private int year;
+	private String condition;
+	private String location;
+	
+	//constructors
+	
+	public Book(String isbn, String title) {
+		this.isbn = isbn;
+		this.title = title;
+		this.condition = "new";
+	}
+	
+	public Book(String author, String title, String isbn) {
+		this(isbn, title);
+		this.author = author;
+	}
+	
+	public Book(String author, String title, String isbn, int year) {
+		this(isbn, title);
+		this.author = author;
+		this.year = year;
+	}
+	
+	public Book(String isbn, String title, String author, int year, String location) {
+		this(author, title, isbn, year);
+		this.location = location;
+	}
+	
+	//setter and getter methods
+	
+	public void setIsbn(String isbn){
+		this.isbn = isbn;
+	}
+	
+	public String getIsbn(){
+		return isbn;
+	}
+	
+	public void setTitle(String title){
+		this.title = title;
+	}
+	
+	public String getTitle(){
+		return title;
+	}
+	
+	public void setAuthor(String author){
+		this.author = author;
+	}
+	
+	public String getAuthor(){
+		return author;
+	}
+	
+	public void setYear(int year){
+		this.year = year;
+	}
+	
+	public int getYear(){
+		return year;
+	}
+	
+	public void setCondition(String condition){
+		this.condition = condition;
+	}
+	
+	public String getCondition(){
+		return condition;
+	}
+	
+	public void setLoctation(String location){
+		this.location = location;
+	}
+	
+	public String getLocation(){
+		return location;
+	}
+	
+	
+	//methods
+	
+	public void loanBook(String location) {
+		
+		if (this.location.equalsIgnoreCase("altpatpiercontainer")) {
+			this.location = location;
+		}
+	}
+	
+	public void returnBook() {
+		this.location = "Buecherregal";
+		
+		if(this.condition.equalsIgnoreCase("neu")) {
+			this.condition = "gebraucht";
+		} else if(this.condition.equalsIgnoreCase("gebraucht")) {
+			this.condition = "ausgesondert";
+			this.location = "Altpapiercontainer";
+		}
+	}
+	
+	public String toString() {
+		String output = ("Titel: " + this.title + ","  
+				+ "Autor(en): " +  this.author + ","
+				+ "ISBN: " + this.isbn + ","
+				+ "Veröffentlicht: " + this.year + ","
+				+ "Zustand: " + this.condition + ","
+				+ "(Standort: " +this.location + ")"
+				);
+		
+		return output;
+	}
+	
+
+}
---
+++ Semester_2/src/vorfuehrAufgabe1/TestBook.java
@@ -0,0 +1,42 @@
+package vorfuehrAufgabe1;
+
+public class TestBook {
+
+	
+	public static void main(String[] args) {
+		// 2.a Buch anlegen und Basistests durchführen   
+		Book myBook = new Book("Andreas Riener", "Sensor-Actuator Supported Implicit Interaction in Driver Assistance Systems", "978-3-8348-0963-6", 2009);
+		
+		System.out.println(myBook.toString());  
+		myBook.loanBook("Ingrid Stahl");
+		System.out.println(myBook.toString());   
+		myBook.returnBook();   
+		System.out.println(myBook.toString());   
+		myBook.loanBook("Andreas");   
+		System.out.println(myBook.toString());   
+		myBook.returnBook();   
+		System.out.println(myBook.toString());
+		
+		// Ein ausgesondertes Buch kann nicht mehr entlehnt werden!    
+		myBook.loanBook("Walter");   
+		System.out.println(myBook.toString());   
+		myBook.returnBook();   
+		System.out.println(myBook.toString());
+		
+		
+		/*
+		// 2.b Prüfung der ISBN-Nummer   
+		// Angabe einer fehlerhaften ISBN-Nummer (die aktuell gespeicherte ISBN-Nummer bleibt)   
+		myBook.setIsbn("1-567-67878-1");    
+		System.out.println(myBook.getIsbn());   
+		// Korrektes aktualisieren der ISBN-Nummer   
+		myBook.setIsbn("1-567-67878-5");    
+		System.out.println(myBook.getIsbn());  
+		// 2.c Prüfen ob 2 Bücher ident sind (Objekte vom Typ Buch; idente ISBN-Nummer)   
+		// 2 verschiedene Objekte vom Typ "Book" mit gleicher ISBN: true   
+		myBook = new Book("Andreas Riener", "Sensor-Actuator Supported Implicit Interaction in Driver Assistance Systems", "978-3-8348-0963-6", 2009);   
+		Book myOtherBook = new Book("Sensor-Actuator Supported Implicit Interaction in Driver Assistance Systems","978-3-8348-0963-6");   
+		System.out.println(myBook.equals(myOtherBook));  */
+	}
+ 
+}
