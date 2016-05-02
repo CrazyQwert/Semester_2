d7ef522f8b9a7a32453d72bec2c5a0ed28e319ca
Arvid (SP3) <a.buschsieweke@outlook.de>
2016-05-02 10:51:04 +0200

    added attributes and setters/getters that go with them
    added name, age, kid

--- Semester_2/src/vorfuehrAufgabe4/Person.java
+++ Semester_2/src/vorfuehrAufgabe4/Person.java
@@ -1,5 +1,31 @@
 package vorfuehrAufgabe4;
 
 public class Person {
+	
+	private String name;
+	private int age;
+	private boolean kid;
+	
+	//getter and setter
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public void setKid(boolean kid) {
+		this.kid = kid;
+	}
+	
+	public boolean isKid() {
+		return this.kid;
+	}
 
 }
