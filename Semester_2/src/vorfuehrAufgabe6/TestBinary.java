5d717397d3b87c905ae7d23c9af6a33b533c4a4b
Arvid <Arvid@Arvids-Tower.Speedport_W723_V_Typ_A_1_01_016>
2016-06-15 02:56:27 +0200

    added another test program

--- Semester_2/src/vorfuehrAufgabe6/TestBinary.java
+++ Semester_2/src/vorfuehrAufgabe6/TestBinary.java
@@ -13,21 +13,42 @@ public class TestBinary {
 
 	public static void main (String[] args)	{
 			
-	// a+(b-c*d)/e
-	// 2+(3-6*12)/8 -> -6
+		// a+(b-c*d)/e
+		// 2+(3-6*12)/8 -> -6
 	
-	IntegerNode a  = new IntegerNode(2);
-	IntegerNode b  = new IntegerNode(3);
-	IntegerNode c  = new IntegerNode(6);
-	IntegerNode d  = new IntegerNode(12);
-	IntegerNode e  = new IntegerNode(8);
-	
-	MultNode mult = new MultNode(c, d);
-	SubNode minus = new SubNode (b, mult);
-	DivNode div   = new DivNode (minus, e);
-	AddNode plus  = new AddNode (a, div);
-	
-	plus.print();
-	System.out.println(" = " +plus.compute());
+		IntegerNode a  = new IntegerNode(2);
+		IntegerNode b  = new IntegerNode(3);
+		IntegerNode c  = new IntegerNode(6);
+		IntegerNode d  = new IntegerNode(12);
+		IntegerNode e  = new IntegerNode(8);
+		
+		MultNode mult = new MultNode(c, d);
+		SubNode minus = new SubNode (b, mult);
+		DivNode div   = new DivNode (minus, e);
+		AddNode plus  = new AddNode (a, div);
+		
+		plus.print();
+		System.out.println(" = " +plus.compute());
+		
+		a  = new IntegerNode(2);
+		b  = new IntegerNode(30);
+		c  = new IntegerNode(2);
+		d  = new IntegerNode(12);
+		e  = new IntegerNode(21);
+		IntegerNode f  = new IntegerNode(7);
+
+		// ( a - b ) * c + d * e / f
+		// ( 2 - 30 ) * 2 + 12 * 21 / 7 -> -20
+		
+		minus = new SubNode(a, b);
+		mult = new MultNode(minus, c);
+		div = new DivNode(e, f);
+		MultNode mult2 = new MultNode(d, div);
+		plus = new AddNode(mult, mult2);
+		
+		plus.print();
+		System.out.println(" = " + plus.compute());
+		
+		
 	}
 }
