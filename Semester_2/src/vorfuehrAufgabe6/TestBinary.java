ab331f5e6df654ecffbb043dad5653d5ed6eca2d
Arvid (SP3) <a.buschsieweke@outlook.de>
2016-06-16 13:26:46 +0200

    added some more test examples

--- Semester_2/src/vorfuehrAufgabe6/TestBinary.java
+++ Semester_2/src/vorfuehrAufgabe6/TestBinary.java
@@ -12,7 +12,9 @@ package vorfuehrAufgabe6;
 public class TestBinary {
 
 	public static void main (String[] args)	{
-			
+		
+		
+		// Beispiel 1
 		// a+(b-c*d)/e
 		// 2+(3-6*12)/8 -> -6
 	
@@ -30,6 +32,11 @@ public class TestBinary {
 		plus.print();
 		System.out.println(" = " +plus.compute());
 		
+		
+		// Beispiel 2
+		// ( a - b ) * c + d * e / f
+		// ( 2 - (-30) ) * 2 + 12 * 21 / 7 -> 100
+
 		a  = new IntegerNode(2);
 		b  = new IntegerNode(-30);
 		c  = new IntegerNode(2);
@@ -37,9 +44,6 @@ public class TestBinary {
 		e  = new IntegerNode(21);
 		IntegerNode f  = new IntegerNode(7);
 
-		// ( a - b ) * c + d * e / f
-		// ( 2 - (-30) ) * 2 + 12 * 21 / 7 -> 100
-		
 		minus = new SubNode(a, b);
 		mult = new MultNode(minus, c);
 		div = new DivNode(e, f);
@@ -49,6 +53,27 @@ public class TestBinary {
 		plus.print();
 		System.out.println(" = " + plus.compute());
 		
+		
+		// Beispiel 3
+		// a - b * ( c + d ) * e / f
+		// 20 - (-5) * ( 3 + 6 ) * 8 / 3
+
+		a  = new IntegerNode(20);
+		b  = new IntegerNode(-5);
+		c  = new IntegerNode(3);
+		d  = new IntegerNode(6);
+		e  = new IntegerNode(8);
+		f  = new IntegerNode(3);
+
+		plus = new AddNode(c, d);
+		mult = new MultNode(b, plus);
+		mult2 = new MultNode(mult, e);
+		div = new DivNode(mult2, f);
+		minus = new SubNode(a, div);
+		
+		minus.print();
+		System.out.println(" = " + minus.compute());
+
 		
 	}
 }
